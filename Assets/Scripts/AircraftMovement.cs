68d1eb1bb265ff2a8665e881b5914f7ade14f75a
Sudiksha <sudiksha.sd1@gmail.com>
2020-04-29 15:53:14 +0530

    Implemented Aircraft 3d movement and rotation

---
+++ Assets/Scripts/AircraftMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AircraftMovement : MonoBehaviour
+{
+    public float rotationSpeed = 15f;
+    public float movementSpeed = 15f;
+    float rotatex = 0;
+    float rotatey = 0;
+    float rotatez = 0;
+    float movement = 0;
+    void Update()
+    {
+        rotatex = Input.GetAxis("RotateX") * rotationSpeed;
+        rotatey = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotatez = Input.GetAxis("RotateZ") * rotationSpeed;
+        movement = Input.GetAxis("Vertical") * movementSpeed;
+        rotatex *= Time.deltaTime;
+        rotatey *= Time.deltaTime;
+        rotatez *= Time.deltaTime;
+        movement *= Time.deltaTime;
+        transform.Translate(new Vector3(0, 0, movement));
+        transform.Rotate(new Vector3(rotatex, 0, 0));
+        transform.Rotate(new Vector3(0, rotatey, 0));
+        transform.Rotate(new Vector3(0, 0, rotatez));
+    }
+}
---
+++ Assets/Scripts/VectorMovement/SpawnSpaceObjects.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnSpaceObjects : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 0; i < 500; i++)
+        {
+            GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            obj.transform.position = Random.insideUnitSphere * 1000;
+        }
+    }
+}
